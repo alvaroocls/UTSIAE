9a6b4bd9820b162eafbe19cdd50fb2f4bcfc7793
Dhimmas <ariateja1973@gmail.com>
2025-04-18 16:55:30 +0700

    Menambahkan ReviewService

---
+++ app/Models/Review.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Review extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'movie_title', 'review', 'rating', 'user_id', 'movie_id' 
+    ];
+    
+
+    // Menambahkan hubungan dengan model User dan Movie
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function movie()
+    {
+        return $this->belongsTo(Movie::class);
+    }
+}
