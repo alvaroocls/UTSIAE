70c6352b6330d0670fbb391da6264a16e773f046
nafizhar ray <63829841+NafizharRayhansyah23@users.noreply.github.com>
2025-04-18 16:50:58 +0700

    update user controller

--- user-service/app/Http/Controllers/UserController.php
+++ user-service/app/Http/Controllers/UserController.php
@@ -59,6 +59,18 @@ class UserController extends Controller
 
         return view('user.profile', compact('user', 'orders'));
     }
+
+    public function getUserProfile($id)
+{
+    $user = User::find($id);
+
+    if (!$user) {
+        return response()->json(['message' => 'User not found'], 404);
+    }
+
+    return response()->json($user);
+}
+
 }
 
     
